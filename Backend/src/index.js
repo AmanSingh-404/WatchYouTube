6d7e29851588b3fe2e9837a27ef294a3a29a67a1
Aman Singh <amanscode2005@gmail.com>
2026-04-23 23:41:23 +0530

    initialize project structure with database constants and backend dependencies

---
+++ Backend/src/index.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from './constants'
+
+// import express from 'express'
+// const app = express()
+
+// ( async () => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+
+//         app.on("error", (err) => {
+//             console.log("error : ", err)
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`app is listen on port : ${process.env.PORT}`)
+//         })
+//     }catch(err){
+//         console.log("error : ", err)
+//     }
+// })
