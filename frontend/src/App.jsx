e550b8c1ce051feae95a0d0a04c3c76b71682bec
Aman Singh <amanscode2005@gmail.com>
2026-05-02 23:01:25 +0530

    initialize React frontend project with Vite and Tailwind CSS support

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-red-500 text-3xl font-semibold'>Watch YouTube</h1>
+
+    </div>
+  )
+}
+
+export default App
